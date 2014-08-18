a6e191036041673a4d7353a0b5383cc4e9401993
Verbalist <enshteynn@ukr.net>
2014-08-18 17:17:37 +0400

    18/08/16:17

--- app/assets/javascripts/turbolinks_transitions.js
+++ app/assets/javascripts/turbolinks_transitions.js
@@ -1,8 +1,4 @@
 function lvl2click(ids) {
-	var last_check = document.getElementById("last_check");
-	if (last_check.innerHTML.split(",")[1] != ids) {
-		last_check.innerHTML = last_check.innerHTML.split(",")[0]+','+ids;
-	}
 	checkBox(ids);
 	subMenu(ids);
 	goodsV(ids);
@@ -137,8 +133,6 @@ function goodsV(ids) {
 };
 
 function MyOnClick(ids) {
-	var last_check = document.getElementById("last_check");
-	last_check.innerHTML = ids;
 	$.ajax({
 		url: "categories.json",
 		dataType : "json",             
