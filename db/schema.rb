3cbff5280889a98d9dfdf77a4b28db6c2ba1563c
Verbalist <enshteynn@ukr.net>
2014-08-19 13:56:21 +0400

    before

--- db/schema.rb
+++ db/schema.rb
@@ -76,6 +76,14 @@ ActiveRecord::Schema.define(version: 20140818144400) do
     t.datetime "avatar_updated_at"
   end
 
+  create_table "locations", force: true do |t|
+    t.string   "address"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "offers", force: true do |t|
     t.string   "title"
     t.text     "description"
