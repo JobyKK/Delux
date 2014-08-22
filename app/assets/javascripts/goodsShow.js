6f7d1c1f2e22afe9c58cf4c1a0f9a3ef21c3ab56
Verbalist <enshteynn@ukr.net>
2014-08-22 14:49:58 +0400

    22/09/13:50

--- app/assets/javascripts/goodsShow.js
+++ app/assets/javascripts/goodsShow.js
@@ -1,10 +1,12 @@
 function goodsShow(ids) {
 	var good = document.getElementById('goods');
+	$(window).scrollTop($("#goods").offset().top);
 	good.innerHTML = '';
 	var el = document.createElement('iframe');
 	good.appendChild(el);
 	el.id = 'iframe';
 	el.style.width = "100%";
-	el.style.height = "350px";
+	el.style.height = "600px";
 	el.src = '/goods/'+ids+'/purchase';
+	
 };
